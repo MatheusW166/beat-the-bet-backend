08131863e67889280e8387608b9fc7ef7c1a279e
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-09-30 19:34:27 -0400

    chore: adds routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
-import { errorMiddlewares } from '@/middlewares';
+import { errorMiddleware } from '@/middlewares';
+import { betRouter, gameRouter, participantRouter } from './routers';
 
 const app = express();
 
 app
   .use(cors())
   .use(express.json())
+  .use('/participants', participantRouter)
+  .use('/games', gameRouter)
+  .use('/bets', betRouter)
   .get('/health', (_req, res) => res.send('OK'))
-  .use(errorMiddlewares.handle);
+  .use(errorMiddleware.handle);
 
 export default app;

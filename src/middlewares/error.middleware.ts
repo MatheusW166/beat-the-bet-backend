873e05bfc87266bb036383ed6de82a1c76f2577e
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-09-30 19:30:56 -0400

    style: eslint format

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,23 +1,28 @@
-import { Request, Response, NextFunction } from "express";
-import httpStatus from "http-status-codes";
-import { DefaultError, UnknownError } from "@/errors";
+import { Request, Response, NextFunction } from 'express';
+import httpStatus from 'http-status-codes';
+import { DefaultError, UnknownError } from '@/errors';
 
 function isDefaultError(error: UnknownError): error is DefaultError {
-	return (error as DefaultError).status !== undefined;
+  return (error as DefaultError).status !== undefined;
 }
 
-function handle(error: UnknownError, _req: Request, res: Response, next: NextFunction): void {
-	if (!error) {
-		next();
-		return;
-	}
+function handle(
+  error: UnknownError,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  if (!error) {
+    next();
+    return;
+  }
 
-	if (isDefaultError(error)) {
-		res.status(error.status).send(error.details);
-		return;
-	}
+  if (isDefaultError(error)) {
+    res.status(error.status).send(error.details);
+    return;
+  }
 
-	res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
 }
 
-export const errorMiddlewares = { handle };
+export const errorMiddleware = { handle };
--- src/middlewares/schema.middleware.ts
+++ src/middlewares/schema.middleware.ts
@@ -1,38 +1,42 @@
-import { NextFunction, Request, Response } from "express";
-import httpStatus from "http-status-codes";
-import { Schema } from "joi";
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status-codes';
+import { Schema } from 'joi';
 
 type MiddlewareFn = (req: Request, res: Response, next: NextFunction) => void;
 
 function validateQuery<T>(schema: Schema<T>): MiddlewareFn {
-	return validate(schema, "query");
+  return validate(schema, 'query');
 }
 
 function validateParams<T>(schema: Schema<T>): MiddlewareFn {
-	return validate(schema, "params");
+  return validate(schema, 'params');
 }
 
 function validateBody<T>(schema: Schema<T>): MiddlewareFn {
-	return validate(schema, "body");
+  return validate(schema, 'body');
 }
 
-function validate<T>(schema: Schema<T>, field: "body" | "params" | "query"): MiddlewareFn {
-	return (req, res, next) => {
+function validate<T>(
+  schema: Schema<T>,
+  field: 'body' | 'params' | 'query',
+): MiddlewareFn {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req[field], { abortEarly: false });
 
-		const { error, value } = schema.validate(req[field], { abortEarly: false });
+    if (error && error.details.length > 0) {
+      res
+        .status(httpStatus.UNPROCESSABLE_ENTITY)
+        .send(error.details.map((detail) => detail.message));
+      return;
+    }
 
-		if (error && error.details.length > 0) {
-			res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.details.map((detail) => detail.message));
-			return;
-		}
-
-		req[field] = value;
-		next();
-	};
+    req[field] = value;
+    next();
+  };
 }
 
-export const schemaMiddlewares = {
-	validateBody,
-	validateParams,
-	validateQuery
+export const schemaMiddleware = {
+  validateBody,
+  validateParams,
+  validateQuery,
 };
